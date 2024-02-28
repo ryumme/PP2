dd88bec9c8f679d0998b20ccfbaf7bbece056acf
ryumme <156985670+ryumme@users.noreply.github.com>
2024-02-29 00:26:06 +0600

    Update 10.py

--- week5/10.py
+++ week5/10.py
@@ -3,7 +3,7 @@ import re
 def snake(s):
     return re.sub("(?!^)(?=[A-Z])", '_', s).lower()
 
-f = open('text.txt', encoding='utf-8')
+f = open('row.txt', encoding='utf-8')
 s = str(f.read())
 ans = snake(s)
 
